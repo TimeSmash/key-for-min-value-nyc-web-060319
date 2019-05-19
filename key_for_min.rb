1029d294b18579e35e6412ea55dc175ed8684bb7
mcumm64 <mcumm64@gmail.com>
2019-05-19 01:20:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,9 +1,17 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
   #Ex. {a => 1, b => 2, c => -1}
+
 def key_for_min_value(name_hash)
-  values = name_hash.collect do |key|
-    key
+  values = name_hash.collect do |key, value|
+    value
   end
-  values
+
+# puts values.sort[0]
+
+  name_hash.each do |key, value|
+    if value == values.sort[0]
+    puts "The lowest value belongs to #{key}"
+    end
+  end
 end
