4fb570af9ff201f9df0574a2a66676e0fdaa8363
mcumm64 <mcumm64@gmail.com>
2019-05-19 01:24:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,17 @@
   #Ex. {a => 1, b => 2, c => -1}
 
 def key_for_min_value(name_hash)
+  
+  if name_hash == {}
+    return nil
+  end
+  
   values = name_hash.collect do |key, value|
     value
   end
-
-# puts values.sort[0]
-
+  
+#use values.sort to have an ordered array of values, then compare index-0 which is the lowest value and see what key it lines up with. If index-0 is a value for a key, then return that key  
+  
   name_hash.each do |key, value|
     if value == values.sort[0]
     return key
