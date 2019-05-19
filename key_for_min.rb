d3ef42c145485904e73926a965c67cde1975f519
mcumm64 <mcumm64@gmail.com>
2019-05-19 01:21:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,7 +11,7 @@ def key_for_min_value(name_hash)
 
   name_hash.each do |key, value|
     if value == values.sort[0]
-    puts "The lowest value belongs to #{key}"
+    return key
     end
   end
 end
